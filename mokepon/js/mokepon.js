7a429f8fcca674bf956faf383e9d69cedbd7ebca
elizabethemepe <115649132+elizabethemepe@users.noreply.github.com>
2022-10-18 17:01:26 -0500

    java script functions

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,13 @@
+function iniciarJuego (){
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+    
+}
+
+
+function seleccionarMascotaJugador(){
+    alert('SELECCIONASTE TU MASCOTA')
+}
+
+
+window.addEventListener('load', iniciarJuego )
