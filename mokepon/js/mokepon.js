a2e733f484f6a2e3b3f70c0758ed3bdc534174e4
elizabethemepe <115649132+elizabethemepe@users.noreply.github.com>
2022-10-19 19:15:46 -0500

    select pet and enemies pet

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -6,20 +6,41 @@ function iniciarJuego (){
 
 
 function seleccionarMascotaJugador(){
-    let inputHipodoge=document.getElementById('hipodoge')
-    let inputCapipepo=document.getElementById('capipepo')
-    let inputRatigueya=document.getElementById('ratigueya')
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
 
     if(inputHipodoge.checked){
-       alert('Seleccionaste a Hipodoge') 
+       spanMascotaJugador.innerHTML = 'Hipodoge'
     }else if(inputCapipepo.checked){
-        alert('Seleccionaste a Capipepo')
+        spanMascotaJugador.innerHTML = 'Capipepo'
     }else if(inputRatigueya.checked){
-        alert('Seleccionaste a Ratigueya')
+        spanMascotaJugador.innerHTML = 'Ratigueya'
     }else{
         alert('Selecciona una mascota')
     }
+
+    seleccionarMascotaEnemigo()
+        let ataqueAleatorio= aleatorio (1,3)
+        let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+        if (ataqueAleatorio == 1) {
+           spanMascotaEnemigo.innerHTML = 'Hipodoge'
+        }else if (ataqueAleatorio == 2){
+            spanMascotaEnemigo.innerHTML = 'Capipepo'
+        }else {
+            spanMascotaEnemigo.innerHTML = 'Ratigueya'
+        }
+    
+
+}
+function seleccionarMascotaEnemigo(){
+
 }
 
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 window.addEventListener('load', iniciarJuego )
